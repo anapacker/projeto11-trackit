98f54830ec539fcaba0473bff62f83778ccb1abc
anapacker <anaamaria.packer@gmail.com>
2023-06-04 20:46:42 -0300

    Carregando os hábitos do usuário, mandando request pra API conforme documentação e exibindo abaixo conforme layout.Caso o usuário não tenha nenhum hábito cadastrado, deve ser exibido o texto conforme layout

--- src/pages/HabitsPage.jsx
+++ src/pages/HabitsPage.jsx
@@ -2,14 +2,41 @@ import styled from "styled-components"
 import Footer from "../components/Footer"
 import CreateHabits from "../components/CreateHabits"
 import NavBar from "../components/NavBar"
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import DataContextProvider from "../context/DataContextProvider";
+import axios from "axios";
 
 export default function HabitsPage() {
     const { token, userPicture, creatingNewHabbit, setCreatingNewHabbit } = useContext(DataContextProvider);
     const [nameHabbit, setNameHabbit] = useState("")
     const [weekdays, setWeekdays] = useState([])
+    const [habbitsList, setHabbitsList] = useState([])
 
+    function isDaySelected(day, habbit) {
+        let daySelected = false;
+
+        for (let i = 0; i < habbit.days.length; i++) {
+            if (habbit.days[i] == day) {
+                daySelected = true
+                break;
+            }
+        }
+        return daySelected
+    }
+
+    const config = {
+        headers: { "Authorization": `Bearer ${token}` }
+    }
+    console.log("componente renderizou")
+    useEffect(() => {
+        const promise = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits`, config)
+        promise.then(resp => {
+            setHabbitsList(resp.data)
+            console.log(resp.data)
+        }
+        )
+
+    }, [])
     return (
         <>
             <NavBar userPicture={userPicture} />
@@ -19,9 +46,29 @@ export default function HabitsPage() {
                     <button data-test="habit-create-btn" className="ButtonCreate" onClick={() => setCreatingNewHabbit(!creatingNewHabbit)}>+</button>
                 </Header>
                 {creatingNewHabbit ? <CreateHabits nameHabbit={nameHabbit} setNameHabbit={setNameHabbit} weekdays={weekdays} setWeekdays={setWeekdays} /> : ""}
-                <span>
-                    Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!
-                </span>
+                {habbitsList.length === 0 ?
+                    <span>
+                        Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!
+                    </span>
+                    :
+                    habbitsList.map(habbit => {
+                        return (
+                            <CreatedHabbit>
+                                <h2>{habbit.name}</h2>
+                                <Weekdays>
+                                    <ButtonDay propsStyledDay={isDaySelected(0, habbit)}>D</ButtonDay>
+                                    <ButtonDay propsStyledDay={isDaySelected(1, habbit)}>S</ButtonDay>
+                                    <ButtonDay propsStyledDay={isDaySelected(2, habbit)}>T</ButtonDay>
+                                    <ButtonDay propsStyledDay={isDaySelected(3, habbit)}>Q</ButtonDay>
+                                    <ButtonDay propsStyledDay={isDaySelected(4, habbit)}>Q</ButtonDay>
+                                    <ButtonDay propsStyledDay={isDaySelected(5, habbit)}>S</ButtonDay>
+                                    <ButtonDay propsStyledDay={isDaySelected(6, habbit)}>S</ButtonDay>
+                                </Weekdays>
+                            </CreatedHabbit>
+                        )
+                    })
+                }
+
             </HabtisContainer>
 
             <Footer />
@@ -68,4 +115,35 @@ const Header = styled.div`
         margin: 80px 0 20px 0;
     }
 `
-
+const CreatedHabbit = styled.div`
+    background-color: white;
+    width: 90%;
+    padding-left: 15px;
+    margin-bottom: 15px;
+    border-radius: 3px;
+    h2{
+        margin-top:15px;
+        font-size: 20px;
+        font-family: 'Roboto';
+    }
+    `
+const Weekdays = styled.div`
+    display: flex;
+    margin-top: 13px;
+    margin-right: 50px;   
+    margin-bottom:15px;    
+`
+const ButtonDay = styled.button`
+    background-color:${(props) => (props.propsStyledDay ? '#CFCFCF' : '#ffffff')};
+    border-radius: 3px;
+    border: 1px solid ${(props) => (props.propsStyledDay ? '#CFCFCF' : '#D4D4D4')};
+    color:${(props) => (props.propsStyledDay ? '#FFFFFF' : '#CFCFCF')} ;
+    width: 30px;
+    height: 30px;
+    display:flex;
+    justify-content: center;
+    align-items:center;
+    font-size: 20px;
+    font-weight: 400;
+    margin-right: 5px;
+`
